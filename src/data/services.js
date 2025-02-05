3280f664cec839903b0879360a0b313f7f7533b8
Anouar <anouarchinoune100@gmail.com>
2025-02-05 20:27:31 +0100

    New features Added

---
+++ src/data/services.js
@@ -0,0 +1,7 @@
+export  const services = [
+    {id:1, name:"FrontEnd"},
+    {id:2, name:"Backend"},
+    {id:3, name:"Customer Service"},
+    {id:4, name:"Web Design"},
+    {id:5, name:"Cloud Computing"}
+]
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,29 +1,116 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { countries } from '../data/countries';
+import { services } from '../data/services';
+import { genders } from '../data/genders';
 
 export default function Form() {
+    const [firstName, setFirstName] = useState('');
+    const [middleName, setMiddleName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [phone, setPhone] = useState('');
+    const [gender, setGender] = useState('None');
+    const [country, setCountry] = useState('None');
+    const [selectedServices, setSelectedServices] = useState([]);
+
+    const handleServiceChange = (e) => {
+        const { value, checked } = e.target;
+        if (checked) {
+            setSelectedServices([...selectedServices, value]);
+        } else {
+            setSelectedServices(selectedServices.filter(service => service !== value));
+        }
+    };
+
+    function submitInfo(e) {
+        e.preventDefault();
+        console.log(`Firstname: ${firstName} \nMiddle Name: ${middleName} \nLast Name: ${lastName} \nEmail: ${email} \nGender: ${gender} \nServices: ${selectedServices} \nCountry: ${country}`);
+    }
+
     return (
-        <>
-            <form action="">
-                <div className="justify-centent-center xs-12 md-8 lg-6">
-                    <div className="form-header bg-primary">
-                        Registration Form
-                    </div>
-                    <div className="form">
-                        <div class="mb-3">
-                            <label for="FirstName" class="form-label">First Name</label>
-                            <input type="text" class="form-control" id="FirstName" required/>
-                        </div>
-                        <div class="mb-3">
-                            <label for="MiddleName" class="form-label" >Middle Name</label>
-                            <input type="text" class="form-control" id="FirstName"/>
-                        </div>
-                        <div class="mb-3">
-                            <label for="FirstName" class="form-label" placeholder="Jhon">First Name</label>
-                            <input type="text" class="form-control" id="FirstName" placeholder="Jhon" required/>
+        <div className="container" onSubmit={submitInfo}>
+            <div className="row">
+                <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                    <div className="card border-0 shadow rounded-3 my-5">
+                        <div className="card-body p-4 p-sm-5">
+                            <h5 className="card-title text-center mb-5 fw-light fs-5">Registration Form</h5>
+                            <form>
+                                <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="firstname" placeholder="" required onChange={(e) => setFirstName(e.target.value)} />
+                                    <label htmlFor="firstname">First Name <span>*</span></label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="middlename" placeholder="" onChange={(e) => setMiddleName(e.target.value)} />
+                                    <label htmlFor="middlename">Middle Name</label>
+                                </div>
+
+                                <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="lastname" placeholder="" onChange={(e) => setLastName(e.target.value)} required />
+                                    <label htmlFor="lastname">Last Name <span>*</span></label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="email" className="form-control" id="email" placeholder="example@mail.com" onChange={(e) => setEmail(e.target.value)} required />
+                                    <label htmlFor="email">E-mail Address <span>*</span></label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                                    <label htmlFor="floatingPassword">Password</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="phone" placeholder="(000) 000-0000" onChange={(e) => setPhone(e.target.value)} />
+                                    <label htmlFor="phone">Phone Number</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <h5>Gender</h5>
+                                    <div className="form-check">
+                                        {genders.map((item) => {
+                                            return (
+                                                <div key={item.id}>
+                                                    <input className="form-check-input" type="radio" name="gender" value={item.name} onChange={(e) => setGender(e.target.value)} />
+                                                    <label className="form-check-label" htmlFor="gender">{item.name}</label><br />
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <h5>Country</h5>
+                                    <select name="" id="" className='form-select' onChange={(e) => setCountry(e.target.value)}>
+                                        <option value="">Select Country</option>
+                                        {countries.map((item) => {
+                                            return <option key={item.code} value={item.name}>{item.name}</option>;
+                                        })}
+                                    </select>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <h5>Services</h5>
+                                    <div className="form-check">
+                                        {services.map((item) => {
+                                            return (
+                                                <div key={item.id}>
+                                                    <input type="checkbox" value={item.name} className='form-check-input' onChange={handleServiceChange} />
+                                                    <label>{item.name}</label><br />
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+                                <div className="form-check mb-3">
+                                    <input className="form-check-input" type="checkbox" value="" id="rememberPasswordCheck" />
+                                    <label className="form-check-label" htmlFor="rememberPasswordCheck">
+                                        Remember password
+                                    </label>
+                                </div>
+                                <div className="d-grid">
+                                    <button className="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Register</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </form>
-        </>
-    )
+            </div>
+        </div>
+    );
 }
